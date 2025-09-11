84f524e6a051f19d720b1dc26b9e8babc1e4c501
DinhDat <dat.dd216809@sis.hust.edu.vn>
2025-09-11 15:06:49 +0700

    first initial for some thing next time

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "iostream.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+using namespace std;
+
+public void main() {
+std::cout<<"Hello World!"<<endl;
+
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+//
+// Created by DatD on 9/11/2025.
+//
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
